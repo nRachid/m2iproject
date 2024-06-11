85e001c241cdcbc4d8fc5db0c48f5a6590bc0925
Rachid NASER <74897115+nRachid@users.noreply.github.com>
2024-06-11 15:20:10 +0200

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -25,7 +25,7 @@ public class AppTest
         @Test
     public void shouldAnswerWithTrue2()
     {
-        assertTrue( false );
+        assertTrue( true );
     }
         @Test
     public void shouldAnswerWithTrue3()
@@ -35,7 +35,7 @@ public class AppTest
         @Test
     public void shouldAnswerWithTrue4()
     {
-        assertTrue( false );
+        assertTrue( true );
     }
     
 }
